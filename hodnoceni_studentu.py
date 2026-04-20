c54bdca29632080c22931b11a24d8e804ab188f2
Ajla Naprelac <283227@vutbr.cz>
2026-04-20 14:16:26 +0200

    update

--- hodnoceni_studentu.py
+++ hodnoceni_studentu.py
@@ -1,7 +1,7 @@
 class StudentsGrades:
     def __init__(self, scores):
         self.scores = scores
-
+        self._sorted_scores = None
     def get_by_index(self, index):
         return self.scores[index]
 
@@ -37,7 +37,7 @@ class StudentsGrades:
             if n[j] > n[j + 1]:
                 n[j], n[j + 1] = n[j + 1], n[j]
         return scores
-    def  find_sorted(self, scores):
+    def  find_sorted(self, score):
         if self.sorted_scores is None:
             self._sorted_scores= self.get_sorted()
         left = 0
@@ -50,4 +50,21 @@ class StudentsGrades:
                 left= midd + 1
             else: right = midd + 1
         return None
+    def main():
+        results = StudentsGrades([85, 42, 91, 67, 50, 73, 100, 38, 58])
+        print(results.count())
+        for i in range(results.count()):
+            print(f"student{i}: {results.get_by_index(i)} points - {results.get_grade(i)}")
 
+            print(results.finf(100))
+            print(results.get_sorted())
+            from sorting import random_numbers
+            random_results = StudentsGrades(random_numbers(30, 0, 100))
+            print(random_results.count())
+            print(random_results.get_sorted())
+
+            print(results.find_sorted(91))
+            print(results.find_sorted(50))
+            print(results.find_sorted(77))
+    if __name__ == "__main__":
+        main()
